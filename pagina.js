df1d9885ccc3243daf1a9c4dbbada93cb2457c60
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-02 09:21:51 -0300

    Mexendo com rotas request e response

--- pagina.js
+++ pagina.js
@@ -2,12 +2,14 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
   var categoria = req.url;
-  if(categoria = "/tecnologia"){
+  if(categoria == "/tecnologia"){
     res.end("<html><body>Categoria de Tecnologia</body></html>");
   }else if(categoria == "/lol"){
     res.end("<html><body>Categoria League of Legends</body></html>");
+  }else if(categoria == "/pop"){
+    res.end("<html><body>Cantores POP</body></html>");
   }
-
+  res.end("<html><body>PÁGINA INICIAL</body></html>");
 });
 
 server.listen(3000);
