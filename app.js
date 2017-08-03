d4a699f77b16f75fdca22611fd6eeb6578c7b340
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-03 08:54:25 -0300

    Desafio tabuada

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.get('/musica', function(req, res){
   res.send("<html><body>Página de Música</body></html>");
 });
 
+app.get('/tabuada', function(req,res){
+  var mensagem = '<center><h2>TABUADAS</h2>';
+  for(a = 1; a < 11; a++){
+    mensagem += '<hr> Tabuada do ' + a + '<br>';
+    for(b = 0; b < 11; b++){
+      var resultado = a + ' x ' + b + '=' + (a * b) + '<br>';
+      mensagem += resultado;
+    }
+  }
+  mensagem += '<hr>';
+  mensagem += '</center>'
+  res.send(mensagem);
+});
+
 app.listen(3000,function(){
   console.log('Servidor ativo e rodando com Express!');
 });
